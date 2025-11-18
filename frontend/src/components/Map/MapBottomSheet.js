ab90de463f636fc57758e99d961e6014b10880c7
yidj0205 <yidj0205@gmail.com>
2025-11-19 00:02:34 +0900

    드래그 개선

--- frontend/src/components/Map/MapBottomSheet.js
+++ frontend/src/components/Map/MapBottomSheet.js
@@ -80,7 +80,7 @@ const MapBottomSheet = ({
       // 선택 해제 시 list 모드로
       setSheetMode('list');
     }
-  }, [selectedRestaurant, snapIndex]);
+  }, [selectedRestaurant]);
 
   // ResizeObserver로 snap 상태 감지
   useEffect(() => {
@@ -94,6 +94,9 @@ const MapBottomSheet = ({
         // snap index 업데이트
         if (ratio < 0.2) {
           newSnapIndex = 0; // 10%
+          if (snapIndex !== 0 && sheetRef.current) {
+            sheetRef.current.snapTo(({ snapPoints }) => snapPoints[0]);
+          }
         } else if (ratio < 0.7) {
           newSnapIndex = 1; // 50%
         } else {
